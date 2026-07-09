f845c48463595b5c663ad522b79baa7deaf3b517
Alex Ojeda <ojedapizango@gmail.com>
2026-07-08 21:53:06 -0500

    segundo cambio

--- .vercel/output/functions/__server.func/_ssr/client-C7mgT9XD.mjs
+++
@@ -1,5 +0,0 @@
-import { t as createClient } from "../_libs/supabase__supabase-js.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/client-C7mgT9XD.js
-var supabase = createClient("https://lufkhjzhvacpjavjvurg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1ZmtoanpodmFjcGphdmp2dXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNjQ2NTEsImV4cCI6MjA5Nzg0MDY1MX0.raJ-oqekYsgAtZ-DKKJt8w8QFhDLfpnfrFW7qFQlUoo");
-//#endregion
-export { supabase as t };
